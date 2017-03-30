a6f9f64824bacb16cd5518d749de458c6afc1d8b
edwinsventura <eduinventura@gmail.com>
2017-03-29 22:47:11 -0400

    For Event List, added: Item layout, fragment layout, adapter, and fragment

---
+++ app/src/main/java/www/gatherup/com/gatherup/adapters/EventListViewAdapter.java
@@ -0,0 +1,61 @@
+package www.gatherup.com.gatherup.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+//import com.example.seniorproject.test.R;
+import www.gatherup.com.gatherup.R;
+
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
+import www.gatherup.com.gatherup.data.AddressGenerator;
+import www.gatherup.com.gatherup.data.Event;
+
+/**
+ * Created by edwinsventura on 3/25/17.
+ */
+
+public class EventListViewAdapter extends ArrayAdapter<Event> {
+
+    public EventListViewAdapter(Context context, ArrayList<Event> events){
+        super(context, 0, events);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Event event = getItem(position);
+
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_event, parent, false);
+        }
+
+        TextView shortDay = (TextView)convertView.findViewById(R.id.item_event_day_ofw);
+        TextView dayNumber = (TextView)convertView.findViewById(R.id.item_event_day_number);
+        TextView title = (TextView)convertView.findViewById(R.id.item_event_title);
+        TextView location = (TextView)convertView.findViewById(R.id.item_event_address);
+        TextView dayAndTime = (TextView)convertView.findViewById(R.id.item_event_date);
+        TextView category = (TextView)convertView.findViewById(R.id.item_event_category);
+        TextView numberOfPeople = (TextView)convertView.findViewById(R.id.item_event_people);
+
+        shortDay.setText(event.getStartDate().getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.getDefault()) );
+        dayNumber.setText(String.valueOf(event.getEndDate().get(Calendar.DAY_OF_MONTH)) );
+        title.setText(event.getTitle());
+        location.setText(AddressGenerator.getAddressLine(getContext(), event.getLatitude(), event.getLongitude()));
+        dayAndTime.setText(event.getStartDate().getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault()) + " " + event.getStartDate().get(Calendar.HOUR) + ":" + event.getStartDate().get(Calendar.MINUTE) + " " + event.getStartDate().getDisplayName(Calendar.AM_PM, Calendar.SHORT, Locale.getDefault()));
+        category.setText(event.getCategory());
+        numberOfPeople.setText("RSVP: " + String.valueOf( event.getAtendeesList().size()));
+
+
+        return convertView;
+    }
+}
---
+++ app/src/main/java/www/gatherup/com/gatherup/fragments/EventListFragment.java
@@ -0,0 +1,141 @@
+package www.gatherup.com.gatherup.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+//import com.example.seniorproject.test.R;
+import www.gatherup.com.gatherup.R;
+
+import java.util.ArrayList;
+
+import www.gatherup.com.gatherup.adapters.EventListViewAdapter;
+import www.gatherup.com.gatherup.data.Event;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link EventListFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link EventListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class EventListFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM_EVENTLIST = "eventArrayList";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private ArrayList<Event> eventArrayList;
+
+    private OnFragmentInteractionListener mListener;
+
+    public EventListFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment EventListFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static EventListFragment newInstance(ArrayList<Event> eventArrayList) {
+        EventListFragment fragment = new EventListFragment();
+        Bundle args = new Bundle();
+        args.putParcelableArrayList(ARG_PARAM_EVENTLIST, eventArrayList);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            eventArrayList = getArguments().getParcelableArrayList(ARG_PARAM_EVENTLIST);
+
+        }
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_event_list, container, false);
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ListView eventListView = (ListView)view.findViewById(R.id.eventlist_fragment_listview);
+        EventListViewAdapter adapter = new EventListViewAdapter(this.getContext(), eventArrayList);
+
+        eventListView.setAdapter(adapter);
+
+
+        //TODO
+        eventListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+        //TextView textView = (TextView)view.findViewById(R.id.test_text_view);
+        //textView.setText(mParam1 + " " + mParam2);
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
