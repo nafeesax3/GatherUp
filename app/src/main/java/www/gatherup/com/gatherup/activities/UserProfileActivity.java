d3abb8292fae8c4fab29508510d458704f4a2063
edwinsventura <eduinventura@gmail.com>
2017-04-10 12:01:20 -0400

    Added basic Profile Activity

---
+++ app/src/main/java/www/gatherup/com/gatherup/activities/UserProfileActivity.java
@@ -0,0 +1,15 @@
+package www.gatherup.com.gatherup.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import www.gatherup.com.gatherup.R;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
